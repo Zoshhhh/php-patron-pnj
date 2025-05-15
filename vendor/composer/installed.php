96562b143f54e6ec80a1b4bef7b9efe985c86140
zoshhhh <129791585+Zoshhhh@users.noreply.github.com>
2025-05-15 17:48:08 +0200

    update vender/

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kevinstacchetti/php-patron-pnj',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6324864bc5b7637888a135ee6914bbb46b05e1e5',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kevinstacchetti/php-patron-pnj' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6324864bc5b7637888a135ee6914bbb46b05e1e5',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

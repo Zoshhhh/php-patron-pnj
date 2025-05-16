334764524c96df6de1f0190dfe3a50cfe8638f1c
Fernando <facoubot2002@gmail.com>
2025-05-16 09:40:06 +0200

    Aucune modification apportée au code

--- public/views/personnage/index.php
+++ public/views/personnage/index.php
@@ -22,8 +22,8 @@ if (isset($_GET['supprimer'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste des Personnages</title>
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/cards.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/cards.css">
 </head>
 <body>
     <div class="characters-list">

3828bfa6c87ce5b4bb5b0a9e2f83e8d0e27abd0e
zoshhhh <129791585+Zoshhhh@users.noreply.github.com>
2025-05-16 08:49:52 +0200

    clear card

--- public/personnages.php
+++ public/personnages.php
@@ -23,6 +23,7 @@ if (isset($_GET['supprimer'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste des Personnages</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/cards.css">
 </head>
 <body>
     <div class="characters-list">
